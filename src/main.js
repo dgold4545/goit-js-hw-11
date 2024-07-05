b121cdfba9d3e9347935df4b3bbb0816a039b78f
DGOLD7788 <80936904545dgold@gmail.com>
2024-07-05 15:20:37 +0300

    add new code

--- src/main.js
+++ src/main.js
@@ -1,19 +1,77 @@
-const refs = {
+// ?Форма пошуку
+
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+const refsEl = {
   form: document.querySelector(".js-form"),
   list: document.querySelector(".js-list"),
 };
-// Форма пошуку міститься в HTML-документі. Користувач буде вводити рядок для пошуку в текстове поле, а за сабмітом форми необхідно виконувати HTTP-запит із цим пошуковим рядком.
 
-refs.form.addEventListener("submit", handlerSubmit);
+refsEl.form.addEventListener("submit", handlerSubmit);
 
 function handlerSubmit(event) {
   event.preventDefault();
 
-  const thisForm = event.currentTarget;
-  const inputValue = thisForm.elements.query.value;
-  const normalizeInputValue = inputValue.trim().toLowerCase();
+  const thisCurrentTarget = event.currentTarget;
 
-  if (!normalizeInputValue) {
+  const normalizedInputValue = thisCurrentTarget.elements.query.value
+    .trim()
+    .toLowerCase();
+
+  if (!normalizedInputValue) {
     return;
   }
+
+  //? HTTP-запити
+
+  createFetchRequest(normalizedInputValue)
+    .then(data => {
+      console.log(data.hits);
+      if (!data.hits.length) {
+        iziToast.error({
+          title: "Caution",
+          position: "topRight",
+          message:
+            "Sorry, there are no images matching your search query. Please try again!",
+        });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+const API_KEY = "44734756-d98c11aebccc1bae684f0851d";
+
+function createFetchRequest(userRequest) {
+  const BASE_URL = "https://pixabay.com";
+  const END_POINT = "/api/";
+  const options = new URLSearchParams({
+    key: API_KEY,
+    q: userRequest,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true,
+  });
+
+  const URL = `${BASE_URL}${END_POINT}?${options}`;
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  return fetch(URL, { options, requestOptions }).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
