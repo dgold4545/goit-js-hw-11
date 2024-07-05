7f47da493c9d86393e4e9b8e0b231a11a3daf46a
DGOLD7788 <80936904545dgold@gmail.com>
2024-07-05 21:54:33 +0300

    add new code

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -10,26 +10,26 @@ export function createItemMarkup(data) {
     downloads,
   } = data;
   return `<li class="list__card">
-            <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}"></a>
-            <ul>
-              <li>
-                <p>Likes</p>
-                <p>${likes}</p>
-              </li>
-              <li>
-                <p>Views</p>
-                <p>${views}</p>
-              </li>
-              <li>
-                <p>Comments</p>
-                <p>${comments}</p>
-              </li>
-              <li>
-                <p>Downloads</p>
-                <p>${downloads}</p>
-              </li>
-            </ul>
-          </li>`;
+<a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}"></a>
+<ul>
+<li class="list__card-headers">
+<h3>Likes</h3>
+<h4>${likes}</h4>
+</li>
+<li class="list__card-headers">
+<h3>Views</h3>
+<h4>${views}</h4>
+</li>
+<li class="list__card-headers">
+<h3>Comments</h3>
+<h4>${comments}</h4>
+</li>
+<li class="list__card-headers">
+<h3>Downloads</h3>
+<h4>${downloads}</h4>
+</li>
+</ul>
+</li>`;
 }
 
 export function createListMarkup(arr) {
